f8eb338f216dfa9e5677235d4fa14285d912a9dc
palmachris7 <palmachristopher7@hotmail.com>
2020-08-02 23:28:09 -0500

    Commit3

--- src/main/java/com/boticaconfianza/boticasconfianzaalmacen/App.java
+++ src/main/java/com/boticaconfianza/boticasconfianzaalmacen/App.java
@@ -1,58 +1,26 @@
 package com.boticaconfianza.boticasconfianzaalmacen;
+
 import javafx.application.Application;
-import javafx.event.EventHandler;
-import javafx.scene.Scene;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import javafx.event.EventHandler;
-import javafx.stage.StageStyle;
 
 /**
  * JavaFX App
  */
 public class App extends Application {
-    private double xOffset = 0;
-    private double yOffset = 0;
+    
     private static Scene scene;
 
-     public static void main(String[] args) {
-        launch();
-    }
-     
     @Override
     public void start(Stage stage) throws IOException {
         scene = new Scene(loadFXML("login"), 640, 480);
         stage.setScene(scene);
-        stage.initStyle(StageStyle.UNDECORATED);
         stage.show();
-        BorderPane root = new BorderPane();
-                
-        
-        
-        root.setOnMousePressed(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent event) {
-                xOffset = event.getSceneX();
-                yOffset = event.getSceneY();
-            }
-        });
-        root.setOnMouseDragged(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent event) {
-                stage.setX(event.getScreenX() - xOffset);
-                stage.setY(event.getScreenY() - yOffset);
-            }
-        });
     }
-    
-    
-    
 
     static void setRoot(String fxml) throws IOException {
         scene.setRoot(loadFXML(fxml));
@@ -63,9 +31,8 @@ public class App extends Application {
         return fxmlLoader.load();
     }
 
-    
-    
-    
-   
+    public static void main(String[] args) {
+        launch();
+    }
 
 }
