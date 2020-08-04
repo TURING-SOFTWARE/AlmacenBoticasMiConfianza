0e1f20c85cb6b6613cbb315363b5b5e1f662a7ea
palmachris7 <palmachristopher7@hotmail.com>
2020-08-04 15:39:00 -0500

    Version estable

--- src/main/java/Conection/ConexionBD.java
+++ src/main/java/Conection/ConexionBD.java
@@ -1,43 +1,20 @@
-
 package Conection;
 
 import java.sql.*;
 
 public class ConexionBD {
     
-    Connection con;
     
-    public ConexionBD(){
-        try{
+      Connection conn = null;
+    public static Connection conDB()
+    {
+        try {
             Class.forName("com.mysql.jdbc.Driver");
-            ///con = DriverManager.getConnection("jdbc:mysql://localhost:3306/almacenmiconfianza","root","root");
-            con = DriverManager.getConnection("jdbc:mysql://localhost/almacenmiconfianza?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC","root","root");
-        }catch(Exception e){
-            System.err.println("Error: "+e);
-        }
-        
-    }
-    
-    public static void main(String[] args){
-        ConexionBD cn=new ConexionBD();
-        Statement st;
-        ResultSet rs;
-        try{
-            st= cn.con.createStatement();
-            rs=st.executeQuery("select * from usuarios");
-            while (rs.next()){
-                System.out.println(rs.getInt("id")+ " " + rs.getString("username") + " " + rs.getString("cotrasena")
-                + " "+ rs.getString("recuperacion"));
-        }
-            cn.con.close();
-        }catch(Exception e){
-            System.err.println("Error: "+e);
-            
-        }
-        
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost/almacenmiconfianza?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC","root","root");
+            return con;
+        } catch (ClassNotFoundException | SQLException ex) {
+            System.err.println("ConnectionUtil : "+ex.getMessage());
+           return null;
         }
     }
-    
-    
-    
-
+}
--- src/main/java/com/boticaconfianza/boticasconfianzaalmacen/App.java
+++ src/main/java/com/boticaconfianza/boticasconfianzaalmacen/App.java
@@ -7,30 +7,56 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import javafx.event.EventHandler;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.StageStyle;
 
 /**
  * JavaFX App
  */
 public class App extends Application {
-    
+    private double xOffset = 0;
+    private double yOffset = 0;
+
     private static Scene scene;
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("login"), 380, 480);
+         Parent root = FXMLLoader.load(getClass().getResource("/fxml/Login.fxml"));
+         stage.initStyle(StageStyle.UNDECORATED);
+        stage.setMaximized(false);
+        
+        
+        
+        //grab your root here
+        root.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                xOffset = event.getSceneX();
+                yOffset = event.getSceneY();
+            }
+        });
+
+        //sorry about that - Windows defender issue.
+        //move around here
+        root.setOnMouseDragged(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                stage.setX(event.getScreenX() - xOffset);
+                stage.setY(event.getScreenY() - yOffset);
+            }
+        });
+        
+        
+        
+        
+        scene = new Scene(root, 340, 440);
         stage.setScene(scene);
         stage.show();
         
     }
 
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
-
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
-    }
+    
 
     public static void main(String[] args) {
         launch();
