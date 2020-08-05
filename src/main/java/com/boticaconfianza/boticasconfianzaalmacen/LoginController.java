99b61d19b48b4b97806453bf87c871b6f68356fb
palmachris7 <palmachristopher7@hotmail.com>
2020-08-04 22:58:06 -0500

    4 agosto

--- src/main/java/com/boticaconfianza/boticasconfianzaalmacen/LoginController.java
+++ src/main/java/com/boticaconfianza/boticasconfianzaalmacen/LoginController.java
@@ -20,6 +20,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
@@ -37,6 +38,12 @@ public class LoginController implements Initializable {
     @FXML
     private Label lblErrors;
     
+//    @FXML
+//    private FontAwesomeIcon btnclose;
+    
+     @FXML
+    private Label lblRecup;
+
     @FXML
     private JFXPasswordField txtPass;
 
@@ -88,6 +95,19 @@ public class LoginController implements Initializable {
             }
         }
 
+    }
+    
+    
+    
+        @FXML
+    void recuperar(MouseEvent event) throws IOException {
+        Node node = (Node) event.getSource();
+                    Stage stage = (Stage) node.getScene().getWindow();
+                    stage.close();
+                    Scene scene = new Scene(FXMLLoader.load(getClass().getResource("/fxml/recuperacion.fxml")));
+                    stage.setScene(scene);
+                    stage.show();
+        
     }
 
     public LoginController() {
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,10 +3,11 @@ module com.boticaconfianza.boticasconfianzaalmacen {
     requires javafx.fxml;
     requires java.base;
     requires java.sql;
-  
+    requires com.jfoenix;
+    
     
     
     opens com.boticaconfianza.boticasconfianzaalmacen to javafx.fxml;
     exports com.boticaconfianza.boticasconfianzaalmacen;
-    requires com.jfoenix;
+    
 }
