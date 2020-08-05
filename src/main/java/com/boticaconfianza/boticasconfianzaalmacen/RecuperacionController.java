fe05d6d83a595b3a8f5642be69b36adad004bb03
palmachris7 <palmachristopher7@hotmail.com>
2020-08-05 13:29:52 -0500

    5 agosto

--- src/main/java/com/boticaconfianza/boticasconfianzaalmacen/RecuperacionController.java
+++ src/main/java/com/boticaconfianza/boticasconfianzaalmacen/RecuperacionController.java
@@ -5,9 +5,17 @@
  */
 package com.boticaconfianza.boticasconfianzaalmacen;
 
+import com.jfoenix.controls.JFXButton;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -16,9 +24,21 @@ import javafx.fxml.Initializable;
  */
 public class RecuperacionController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+   
+    @FXML
+    private JFXButton lblreturn;
+
+    @FXML
+    void regresar(MouseEvent event) throws IOException {
+        Node node = (Node) event.getSource();
+                    Stage stage = (Stage) node.getScene().getWindow();
+                    stage.close();
+                    Scene scene = new Scene(FXMLLoader.load(getClass().getResource("/fxml/login.fxml")));
+                    stage.setScene(scene);
+                    stage.show();
+        
+        
+    }
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
