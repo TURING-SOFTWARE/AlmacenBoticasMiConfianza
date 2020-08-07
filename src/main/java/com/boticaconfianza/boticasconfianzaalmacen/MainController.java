44f5607fc4fd65286eb7bc42a9861c5a29f49c4f
palmachris7 <palmachristopher7@hotmail.com>
2020-08-06 23:25:48 -0500

    commit 10

--- src/main/java/com/boticaconfianza/boticasconfianzaalmacen/MainController.java
+++ src/main/java/com/boticaconfianza/boticasconfianzaalmacen/MainController.java
@@ -9,12 +9,15 @@ import com.jfoenix.controls.JFXButton;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
@@ -43,6 +46,22 @@ public class MainController implements Initializable {
 
     @FXML
     private JFXButton btnCerrar;
+    
+    @FXML
+    private JFXButton btnPrincipal;
+
+    @FXML
+    private JFXButton btnProductos;
+
+    @FXML
+    private JFXButton btnInventario;
+
+    @FXML
+    private JFXButton btnReportes;
+
+    @FXML
+    private JFXButton btnMantenimiento;
+
 
     @FXML
     void cerrar(MouseEvent event) {
@@ -65,7 +84,7 @@ public class MainController implements Initializable {
     }
         
        
-    
+  
 
     @FXML
     void minimizar(MouseEvent event) {
@@ -74,9 +93,47 @@ public class MainController implements Initializable {
         stage.setIconified(true);
 
     }
+    
+    
+    @FXML
+    void inventario(MouseEvent event) {
+
+    }
 
     @FXML
+    void mantenimiento(MouseEvent event) {
 
+    }
+
+       @FXML
+    void principal(MouseEvent event) {
+
+    }
+
+    @FXML
+    void productos(MouseEvent event) {
+
+    }
+
+    @FXML
+    void reportes(MouseEvent event) {
+
+    }  
+
+    private void loadpage(String page){
+        Parent root=null;
+        try {
+            root = FXMLLoader.load(getClass().getResource(page+".fxml"));
+        } catch (IOException ex) {
+            Logger.getLogger(MainController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    
+    }
+    
+    
+    
+    
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
